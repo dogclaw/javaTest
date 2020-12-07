802f6cf4117ae6780fea3ddf104fa5d506f8aa9b
dogclaw <dogclaw@163.com>
2020-12-07 17:27:24 +0800

    lll

---
+++ src/helloWorld.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloWorld");
+        Date  date = new Date();
+        System.out.println(date);
+        List<Integer>list=new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        System.out.println(list);
+        for(Integer a:list)
+        {System.out.println(a);}
+        String[] string1=new String[list.size()];
+        list.toArray(string1);
+        for (int i=0;i< string.length;i++)
+        {
+            System.out.println(string[i]);
+        }
+        Iterator<Integer> ite= list.iterator();
+        while (ite.hasNext())
+        {
+            System.out.println(ite.next());
+        }
+
+    }
+}
