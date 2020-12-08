b8b83242c0609276cbdcd31558c3feed589d41fc
dogclaw <dogclaw@163.com>
2020-12-08 08:58:38 +0800

    aa

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -3,23 +3,22 @@ import java.util.*;
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("helloWorld");
-        Date  date = new Date();
+        Date date = new Date();
         System.out.println(date);
-        List<Integer>list=new ArrayList<Integer>();
+        List<Integer> list = new ArrayList<Integer>();
         list.add(1);
         list.add(2);
         System.out.println(list);
-        for(Integer a:list)
-        {System.out.println(a);}
-        String[] string1=new String[list.size()];
-        list.toArray(string1);
-        for (int i=0;i< string.length;i++)
-        {
-            System.out.println(string[i]);
+        for (Integer a : list) {
+            System.out.println(a);
         }
-        Iterator<Integer> ite= list.iterator();
-        while (ite.hasNext())
-        {
+        String[] string1 = new String[list.size()];
+        list.toArray(string1);
+        for (int i = 0; i < string1.length; i++) {
+            System.out.println(string1[i]);
+        }
+        Iterator<Integer> ite = list.iterator();
+        while (ite.hasNext()) {
             System.out.println(ite.next());
         }
 
